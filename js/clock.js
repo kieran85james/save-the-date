e760d1c605422f7bdd911f008e2ea258f1727def
Kieran James <49641524+kieran85james@users.noreply.github.com>
2026-05-10 21:29:42 +0100

    Fix issue with interval running before initalize

--- js/clock.js
+++ js/clock.js
@@ -1,49 +1,33 @@
 $(document).ready(function() {
-  let clock;
 
   // Current time in London timezone
   let currentDate = moment.tz("Europe/London");
 
   // Target date in London timezone
-  // let targetDate = moment.tz("2026-05-23 00:00", "Europe/London");
-  let targetDate = moment.tz("2026-05-10 21:30", "Europe/London");
+  let targetDate = moment.tz("2026-05-23 00:00", "Europe/London");
 
   // Difference in seconds
   let diff = targetDate.diff(currentDate, "seconds");
 
-  if (diff <= 0) {
+  // Prevent negative starting values
+  diff = Math.max(diff, 0);
 
-    clock = $(".clock").FlipClock(0, {
-      clockFace: "DailyCounter",
-      countdown: true,
-      autostart: false
-    });
+  // Create clock
+  let clock = $(".clock").FlipClock(diff, {
+    clockFace: "DailyCounter",
+    countdown: true,
+    callbacks: {
 
-    console.log("Date has already passed!");
+      stop: function() {
 
-  } else {
+        // Force display to remain at 0
+        clock.setTime(0);
 
-    clock = $(".clock").FlipClock(diff, {
-      clockFace: "DailyCounter",
-      countdown: true,
-      callbacks: {
-
-        interval: function() {
-
-          // Prevent negative countdown
-          if (clock.getTime().time <= 0) {
-            clock.setTime(0);
-            clock.stop();
-          }
-
-        },
-
-        stop: function() {
-          console.log("Timer has ended!");
-        }
+        console.log("Timer has ended!");
 
       }
-    });
 
-  }
+    }
+  });
+
 });
